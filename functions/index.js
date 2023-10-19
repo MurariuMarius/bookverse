4650cc75bcce5d6b045d6a26e1f91f266722345b
Marius Murariu <marius.murariu@student.upt.ro>
2023-10-19 22:32:19 +0300

    Add book info to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,60 @@
-const {onCall, HttpsError} = require("firebase-functions/v2/https");
-const {logger} = require("firebase-functions/v2");
+const { onCall, HttpsError } = require('firebase-functions/v2/https')
+const { logger } = require('firebase-functions/v2')
 
-const bookConditions = ['New', 'As New', 'Good', 'Fair', 'Poor'];
+const { initializeApp } = require('firebase-admin/app')
+const { getFirestore } = require('firebase-admin/firestore')
+
+const app = initializeApp()
+
+const firestoreService = getFirestore()
+
+const bookConditions = ['New', 'As New', 'Good', 'Fair', 'Poor']
 
 exports.createOffer = onCall(async (request) => {
   const getBookDetails = async (ISBN) => {
-    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${ISBN}+isbn`);
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:${ISBN}`)
     const books = await response.json();
 
     if (books.totalItems != 0) {
 
-      const book = books.items[0].volumeInfo;
+      const book = books.items[0].volumeInfo
 
-      const authors = book.authors;
-      const title = book.title;
+      const authors = book.authors
+      const title = book.title
   
-      return { title, authors };
+      return { title, authors }
     } else {
-      throw new Error('ISBN not found');
+      throw new Error('ISBN not found')
     }
   }
 
-  const validISBN = (ISBN) => {
-    return /^[0-9]{13}$/.test(ISBN)
+  const validateBookRequest = (book) => {
+    const validISBN = (ISBN) => {
+      return /^[0-9]{13}$/.test(ISBN)
+    }
+
+    if (!bookConditions.includes(request.data.condition)) {
+      throw new HttpsError('Invalid book condition')
+    }
+  
+    if (!validISBN(request.data.ISBN.trim())) {
+      throw new HttpsError('Invalid ISBN')
+    }
   }
 
-  if (!bookConditions.includes(request.data.condition)) {
-    throw new HttpsError('Invalid book condition')
+  const storeBookIfNotInDB = async (book) => {
+    const snapshot = await firestoreService.collection('books').doc(book.ISBN).get()
+    if (!snapshot.exists) {
+      await firestoreService.collection('books').doc(book.ISBN).set(book)
+    }
   }
-
-  if (!validISBN(request.data.ISBN.trim())) {
-    throw new HttpsError('Invalid ISBN')
-  }
-
+  
   let book = {
     ISBN: request.data.ISBN.trim(),
     condition: request.data.condition
   }
+
+  validateBookRequest(book)
 
   try {
     const verifiedBook = await getBookDetails(book.ISBN)
@@ -48,5 +66,10 @@ exports.createOffer = onCall(async (request) => {
     book = { title: title, authors: authors, ...book }
   }
   
+  try {
+    await storeBookIfNotInDB(book)
+  } catch (err) {
+    logger.log(err.message)
+  }
   return book
 });
