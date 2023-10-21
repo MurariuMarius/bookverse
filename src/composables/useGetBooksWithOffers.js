1b002b30018734b0289e509e7f0848e09ae6cf2e
Marius Murariu <marius.murariu@student.upt.ro>
2023-10-21 18:19:26 +0300

    Improved book data loading time

--- src/composables/useGetBooksWithOffers.js
+++ src/composables/useGetBooksWithOffers.js
@@ -7,19 +7,22 @@ const error = ref('')
 const { getOffersForBook } = useGetOffersForBook()
 
 const getBooksWithOffers = async () => {
+  
   const books = []
-
+  
   const bookSnapshot = await firestoreService.collection('books').get()
   bookSnapshot.forEach(doc => books.push(doc.data()))
-
+  
   const booksWithOffers = new Map();
-
-  for (const book of books) {
-    const offersForBook = await getOffersForBook(book.ISBN)
-    if (offersForBook.length > 0) {
-      booksWithOffers.set(book, offersForBook)
-    }
-  }
+  
+  await Promise.all(
+    books.map(async (book) => {
+      const offersForBook = await getOffersForBook(book.ISBN)
+      if (offersForBook.length > 0) {
+        booksWithOffers.set(book, offersForBook)
+      }
+    })
+  )
 
   return booksWithOffers;
 }
