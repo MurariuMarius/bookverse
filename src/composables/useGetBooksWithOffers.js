06a869bd38dbe390e2eea9722996d1a975053d29
Marius Murariu <marius.murariu@student.upt.ro>
2023-10-23 17:33:07 +0300

    Added search functionality

--- src/composables/useGetBooksWithOffers.js
+++ src/composables/useGetBooksWithOffers.js
@@ -6,17 +6,36 @@ import useGetOffersForBook from '@/composables/useGetOffersForBook'
 const error = ref('') 
 const { getOffersForBook } = useGetOffersForBook()
 
-const getBooksWithOffers = async () => {
+const getBooksWithOffers = async (search) => {
   
+  if (search) {
+    search = search.toLowerCase()
+  }
+
   const books = []
   
   const bookSnapshot = await firestoreService.collection('books').get()
   bookSnapshot.forEach(doc => books.push(doc.data()))
+
+  let filteredBooks = []
+
+  if (search) {
+    books.filter(b => {
+      return b.ISBN === search ||
+      b.authors
+        .some(author => {
+          return author.toLowerCase().split(" ").includes(search);
+        }) ||
+      b.title.toLowerCase().split(" ").includes(search)
+    }).forEach(d => filteredBooks.push(d))
+  } else {
+    filteredBooks = books
+  }
   
   const booksWithOffers = new Map();
   
   await Promise.all(
-    books.map(async (book) => {
+    filteredBooks.map(async (book) => {
       const offersForBook = await getOffersForBook(book.ISBN)
       if (offersForBook.length > 0) {
         booksWithOffers.set(book, offersForBook)
