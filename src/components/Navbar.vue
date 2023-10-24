2dee6e88d9ce088483c8315ffc31c6b4acf571d2
marius.murariu <marius.murariu@student.upt.ro>
2023-10-24 15:11:36 +0300

    Display book orders in shopping cart

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,16 @@
 <template>
+  {{ shoppingCart.getOrderCount() }}
+  <div v-if="showShoppingCart" class="background">
+    <div class="center">
+      <p class="close" @click="toggleShoppingCart">X</p>
+      <Order v-for="order in orders" :key="order" :order="order"/>
+      SHOPPING CART
+    </div>
+  </div>
   <nav>
     <div class="menu">
       <div v-if="user">
+        <img src="@/assets/shopping-cart.svg" alt="shopping-cart" class="shopping-cart" @click="toggleShoppingCart">
         <h2 @click="goToProfile">{{ user.displayName }}</h2>
         <button @click="handleSignout">Sign out</button>
       </div>
@@ -14,18 +23,25 @@
 </template>
 
 <script>
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 import useSignout from '@/composables/useSignout'
 import getUser from '@/composables/getUser'
+import Order from '@/components/Order.vue'
+
+import { shoppingCart } from '@/composables/shoppingCart'
 
 export default {
-  
+  components: { Order },
   setup() {
     const router = useRouter()
     const { user } = getUser()
 
     const { error, signout } = useSignout()
+
+    const showShoppingCart = ref(false)
+    const orders = ref()
 
     const handleSignout = async () => {
       await signout()
@@ -39,7 +55,14 @@ export default {
       router.push({ name: 'profile' })
     }
 
-    return { user, goToProfile, handleLogin, handleSignout }
+    const toggleShoppingCart = () => {
+      if (!showShoppingCart.value) {
+        orders.value = shoppingCart.getOrders()
+      }
+      showShoppingCart.value = !showShoppingCart.value
+    }
+
+    return { shoppingCart, showShoppingCart, orders, user, goToProfile, handleLogin, handleSignout, toggleShoppingCart }
   }
 }
 </script>
@@ -69,4 +92,33 @@ nav {
   border-radius: 15px;
   background-image: linear-gradient(to bottom right, #69c606, var(--dark-green));
 }
+
+.shopping-cart
+ {
+  height: 50px;
+  margin-right: 15px;
+}
+
+.shopping-cart:hover {
+  cursor: pointer;
+}
+
+.center {
+  margin: 30px auto;
+  max-width: 1000px;
+  width: 90%;
+  background-color: white;
+  border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+}
+.close {
+  align-self: flex-end;
+  margin: 25px 25px 0 0;
+}
+
+.close:hover {
+  cursor: pointer;
+}
+
 </style>
---
+++ src/components/Order.vue
@@ -0,0 +1,104 @@
+<template>
+  <div v-if="showOrder" class="card" @click="removeOrder">
+    <img :src="imageSource" :alt="order.book.title">
+    <div class="book">
+      <h2>{{ order.book.title }}</h2>
+      <h3>{{ authors }}</h3>
+    </div>
+    <div class="details">
+      <h3 class="price"><pre>{{ order.offer.price }}  €</pre></h3>
+      <p class="condition">{{ order.offer.condition }}</p>
+    </div>
+
+  </div>
+</template>
+  
+<script>
+import { shoppingCart } from '@/composables/shoppingCart';
+import useGetBookIcon from '@/composables/useGetBookIcon'
+import { computed, ref } from 'vue'
+
+export default {
+  props: { order: Object },
+  setup(props, { emit }) {
+    const { imageSource, getBookIcon } = useGetBookIcon();
+      
+    const showOrder = ref(true)
+
+    const authors = computed(() => {
+        return props.order.book.authors.join(', ');
+    });
+
+    getBookIcon(props.order.book.imageURL);
+
+    const removeOrder = () => {
+      // TODO
+    }
+
+    return { authors, imageSource, showOrder, removeOrder };
+  },
+}
+</script>
+
+<style scoped>
+.card {
+  align-items: center;
+  display: flex;
+  flex-flow: row;
+  flex-wrap: wrap;
+  width: 90%;
+  background-color: #e8e5e5;
+  border-radius: 25px;
+  justify-content: space-around;
+  margin: 20px auto;
+}
+
+.card:hover {
+  background-color: #dcdcdc;
+  cursor: pointer;
+}
+
+img {
+  margin: 10px auto;
+  height: 150px;
+}
+
+.price {
+  margin: 0;
+  font-weight: 800;
+  font-size: x-large;
+  color: var(--dark-green);
+}
+
+.book {
+  flex-basis: 50%;
+}
+
+.condition {
+  margin: auto 5px;
+  align-items: center;
+  display: flex;
+  flex-flow: column;
+  row-gap: 10px;
+  text-transform: uppercase;
+  font-size: larger;
+  font-weight: 500;
+}
+
+.details {
+  flex-basis: 20%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+p {
+  margin: 0px;
+}
+
+.sold {
+  background-color: var(--light-green);
+}
+
+</style>
