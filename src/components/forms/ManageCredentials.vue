b07461d5c41d73cdaf3770e86f248bfb05045f3a
Marius Murariu <marius.murariu@student.upt.ro>
2023-10-29 20:54:36 +0200

    Reset email

--- src/components/forms/ManageCredentials.vue
+++ src/components/forms/ManageCredentials.vue
@@ -1,6 +1,6 @@
 <template>
   <h2>Reset email</h2>
-  <form @submit.prevent="">
+  <form @submit.prevent="handleResetEmail">
     <input type="email" placeholder="New email" required v-model="email">
     <input type="password" placeholder="Password" required v-model="passwordEmail">
     <button>Reset email</button>
@@ -16,17 +16,21 @@
 </template>
 
 <script>
-import { EmailAuthProvider, getAuth, reauthenticateWithCredential, updatePassword } from 'firebase/auth'
+import { EmailAuthProvider, getAuth, reauthenticateWithCredential, updateEmail, updatePassword } from 'firebase/auth'
 import { ref } from 'vue'
 
 import useNotification from '@/composables/utils/useNotification'
+import { getFunctions, httpsCallable } from 'firebase/functions'
 
 export default {
   setup() {
     const auth = getAuth()
 
+    const functions = getFunctions()
+
     const { showNotification, notificationMessage, notificationType, toggleNotification } = useNotification()
 
+    const email = ref('')
     const password = ref('')
     const passwordEmail = ref('')
     const newPassword = ref('')
@@ -42,23 +46,48 @@ export default {
         )
       )
     }
+    
+    const handleResetEmail = async () => {
+      try {
+        await reauthenticateUser(passwordEmail.value)
+      } catch (err) {
+        toggleNotification(err.message, 'error', 2000)
+        return
+      }
+
+      const resetEmail = httpsCallable(functions, 'manageUser-resetEmail')
+
+      try {
+        await resetEmail({ uid: auth.currentUser.uid, email: email.value })
+        toggleNotification('Email reset.', 'success', 2000)
+      } catch (err) {
+        toggleNotification(err.message, 'error', 2000)
+      }
+    }
 
     const handleResetPassword = async () => {
       if (newPassword.value !== newPasswordAgain.value) {
         error.value = 'Passwords do not match.'
+        return
       }
 
-      reauthenticateUser(password.value)
+      try {
+        await reauthenticateUser(passwordEmail.value)
+      } catch (err) {
+        toggleNotification(err.message, 'error', 2000)
+        return
+      }
 
       try {
         await updatePassword(auth.currentUser, newPassword.value)
         toggleNotification('Password reset.', 'success', 2000)
       } catch (err) {
         error.value = err.message
+        toggleNotification(err.message, 'error', 2000)
       }
     }
 
-    return { password, passwordEmail, newPassword, newPasswordAgain, error, showNotification, notificationMessage, notificationType, handleResetPassword }
+    return { email, password, passwordEmail, newPassword, newPasswordAgain, error, showNotification, notificationMessage, notificationType, handleResetEmail, handleResetPassword }
   }
 }
 </script>
