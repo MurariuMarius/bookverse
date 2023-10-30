8609dd1828f7a36f0d0e3af0a991846656e85ba5
Marius Murariu <marius.murariu@student.upt.ro>
2023-10-30 14:44:20 +0200

    Changed main page styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,9 +6,12 @@
 
 <style>
 footer {
+  box-sizing: border-box;
+  width: 100%;
+  bottom: 0;
   padding: 10px 5%;
-  background-image: linear-gradient(to top right, #25b800, #69c606);
-  color: #fffafa;
+  background-image: linear-gradient(to top right, #caffc5, #b7ffbf);
+  color: #4f5060;
   font-weight: 400;
 }
 </style>
