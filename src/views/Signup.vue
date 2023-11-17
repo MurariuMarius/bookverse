351c22266fbe88f4122e55d12c446da80b3bb856
Marius Murariu <marius.murariu@student.upt.ro>
2023-11-17 16:54:52 +0200

    Fixed firestore user document creation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -50,7 +50,7 @@ export default {
     const handleSubmit = async () => {
       if (verifyMatchingPasswords()) {
         const res = await signup(email.value, password.value, username.value)
-        firestoreService.collection('users')
+        await firestoreService.collection('users')
           .doc(res.user.uid)
           .set({
             username: username.value,
